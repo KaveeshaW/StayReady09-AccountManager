8765133eedec4e26dfdc74e6e45f2044b65802d6
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-18 09:33:28 -0500

    finished businessAccount

--- src/test/java/com/codedifferently/bankaccountlab/BusinessAccountTest.java
+++ src/test/java/com/codedifferently/bankaccountlab/BusinessAccountTest.java
@@ -1,16 +1,52 @@
 package com.codedifferently.bankaccountlab;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BusinessAccountTest {
+    BusinessAccount buisnessAccount;
+
     @Before
     public void instantiate() {
-
+        buisnessAccount = new BusinessAccount();
     }
 
     @Test
-    public void constructorTest() {
+    public void getNameTest() {
+        String expected = "not set";
 
+        String actual = buisnessAccount.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNameTest() {
+        String expected = "Wachovia";
+
+        buisnessAccount.setName("Wachovia");
+        String actual = buisnessAccount.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getRateTest() {
+        double expected = 0.0;
+
+        double actual = buisnessAccount.getRate();
+
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void setRateTest() {
+        double expected = 12.3;
+
+        buisnessAccount.setRate(12.3);
+        double actual = buisnessAccount.getRate();
+
+        Assert.assertEquals(expected, actual, 0.0);
     }
 }
