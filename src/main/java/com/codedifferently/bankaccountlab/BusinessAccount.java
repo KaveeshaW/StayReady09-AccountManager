7c76fdf8a1d3a0b42d7a76b35fac2c2c53cd447e
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-18 09:44:04 -0500

    finished part 1

--- src/main/java/com/codedifferently/bankaccountlab/BusinessAccount.java
+++ src/main/java/com/codedifferently/bankaccountlab/BusinessAccount.java
@@ -1,5 +1,27 @@
 package com.codedifferently.bankaccountlab;
 
 public class BusinessAccount extends BankAccount{
+    private String companyName;
+    private int taxID;
 
+    public BusinessAccount() {
+        companyName = "not known";
+        taxID = 0;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getTaxID() {
+        return taxID;
+    }
+
+    public void setTaxID(int taxID) {
+        this.taxID = taxID;
+    }
 }
--- src/test/java/com/codedifferently/bankaccountlab/BusinessAccountTest.java
+++ src/test/java/com/codedifferently/bankaccountlab/BusinessAccountTest.java
@@ -5,5 +5,48 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BusinessAccountTest {
+    BusinessAccount businessAccount;
 
+    @Before
+    public void instantiate() {
+        businessAccount = new BusinessAccount();
+    }
+
+    @Test
+    public void getCompanyTest() {
+        String expected = "not known";
+
+        String actual = businessAccount.getCompanyName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setCompanyTest() {
+        String expected = "Wells Fargo";
+
+        businessAccount.setCompanyName("Wells Fargo");
+        String actual = businessAccount.getCompanyName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTaxIDTest() {
+        int expected = 0;
+
+        int actual = businessAccount.getTaxID();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setIDTest() {
+        int expected = 123456789;
+
+        businessAccount.setTaxID(123456789);
+        int actual = businessAccount.getTaxID();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
